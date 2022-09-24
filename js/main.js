0b513be1b90a44c7a0c5487d6429ac4708e1dd1d
Maylon-tech <minatokiroisen77@gmail.com>
2022-09-24 12:36:06 +0900

    Finished the Layout - Start Reponsive

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ const header = document.querySelector('header')
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0)
 })
+
+
+
+
+let heart = document.querySeletorAll(".bx-heart")
+console.log(heart)
+heart.forEach(item => {
+    console.log(item)
+    item.addEventListener("change", () => {
+        heart.classList.toggle("color")
+    })
+})
